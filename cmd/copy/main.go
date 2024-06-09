283a07ed3cc2530c5461a1f7f6651473e9d8565d
Josh Burns <joshyburnss@gmail.com>
2024-06-09 12:57:09 +1000

    initialize cli flags

--- cmd/copy/main.go
+++ cmd/copy/main.go
@@ -2,9 +2,34 @@ package main
 
 import (
 	"fmt"
-	"github.com/spf13/pflag"
+	flag "github.com/spf13/pflag"
+	"os"
 )
 
+var (
+	showHelp     bool
+	showProgress bool
+	recursive    bool
+)
+
+func init() {
+	flag.BoolVarP(&showHelp, "help", "h", false, "Display help message")
+	flag.BoolVarP(&showProgress, "progress", "p", false, "Display progress bar")
+	flag.BoolVarP(&recursive, "recursive", "R", false, "Copy directory recursively")
+
+	// Usage
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [OPTIONS] SRC DEST\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+}
+
 func main() {
-	fmt.Println("copy")
+	flag.Parse()
+
+	// Display help message if asked for it
+	if showHelp {
+		flag.Usage()
+		return
+	}
 }
